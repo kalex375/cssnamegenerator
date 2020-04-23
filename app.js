e579f51433a67fe332d7b92867b8fd72f4acaec4
kalex375 <kalex375@gmail.com>
2020-04-24 00:31:03 +0700

    build

--- app.js
+++ app.js
@@ -1,40 +1,54 @@
 import $ from 'jquery';
 
-const day = ["a good&nbsp;",
-'Wrppaer',
-'Inner',
-'Box',
-'Panel',
-'Content',
-'Body',
-'Container',
-'Outer',
-'Cover',
-'Section',
-'Frame',
-'Module',
-'Block',
-'Set',
-'Group',
+import odoo from './lib/bounty';
+
+const classes = [
+    'Wrppaer',
+    'Inner',
+    'Box',
+    'Panel',
+    'Content',
+    'Body',
+    'Container',
+    'Outer',
+    'Cover',
+    'Section',
+    'Frame',
+    'Module',
+    'Block',
+    'Set',
+    'Group',
 ];
 
-let allDays = day.length;
-let firstDay = 0;
-let change = $('.day')
+let from = 'click me'.toUpperCase();
+let to
+$('.box').text(from);
 
-function changeDay() {
-    change.html(day[firstDay]);
-    firstDay = (firstDay + 1) % allDays;
-};
+$('body').on('click.gen',runGenrator)
 
-let ohYeah = null;
+function runGenrator(){
+        if (classes.length === 0) return
 
-function ohNo() {
-    ohYeah = setInterval(changeDay, 150); //must match css animation
-};
+        $('body').off('click.gen')
 
-/*$('.day').hover(function() {
-    ohNo();
-}, function() {
-    clearInterval(ohYeah);
-});*/
+        let i = Math.floor(Math.random() * classes.length)
+        let to = classes.splice(i, 1)[0].toUpperCase()
+        let maxLenth = Math.max(from.length, to.length)
+    
+        from = fitToCenter(from, maxLenth)
+        to = fitToCenter(to, maxLenth)
+    
+        console.log('"' + from + '"', '"' + to + '"')
+        odoo({ el: '.box', from: from, to: to, animationDelay: 1000 });
+        from = to
+        setTimeout(()=>{
+            $('body').on('click.gen',runGenrator)
+        },2000)
+}
+function fitToCenter(str, len) {
+    if (str.length >= len) return str
+    let s = Math.floor((len - str.length) / 2)
+    str = Array(s+1).join(' ') + str + Array(s+1).join(' ')
+    if (str.length < len) str = str + ' '
+    return str
+}
